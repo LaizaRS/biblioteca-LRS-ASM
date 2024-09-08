a4d37a2af854041a6ccaaa65f45501f32992edf6
Laiza RS <l.aiza.saraiva@gmail.com>
2024-09-08 09:23:46 -0400

    colocando font_color no genero

--- src/componentes/CardLivros/CardLivros.js
+++ src/componentes/CardLivros/CardLivros.js
@@ -12,7 +12,7 @@ const CardLivros = ({ generoSelecionado, autorSelecionado, livros }) => {
         <div className="estante-de-livros">
             {livrosFiltrados.map((livro) => (
                 <div key={livro.id} className='card-livros'>
-                    <h3 className='genero'>{livro.genero.nome}</h3>
+                    <h3 className='genero' style={{color:livro.genero.font_color}}>{livro.genero.nome}</h3>
                     <h4 className='descricao-espaco'>{livro.autor.nome}</h4>
                     <img 
                         className='img-card-livro'
